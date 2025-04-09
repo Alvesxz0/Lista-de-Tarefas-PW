bab750125c9e291f9af9f7b0dc3641b82bba46a6
Alvesxz0 <viniciusviana1538@gmail.com>
2025-04-09 20:03:58 -0300

    Adicionando comentários

--- minha-lista-tarefas/src/App.jsx
+++ minha-lista-tarefas/src/App.jsx
@@ -1,15 +1,15 @@
-import ListaTarefas from './components/ListaTarefas'
+import ListaTarefas from './components/ListaTarefas' //Importando o componente lista de tarefas
 
-import './App.css'
+import './App.css' //Importando o css
 
-function App() {
+function App() { //Constroi o aplicativo
   
   return (
     <>
-      <h1 className='h1'>Gerenciador de Tarefas</h1>
-      <ListaTarefas />
+      <h1 className='h1'>Gerenciador de Tarefas</h1> {/* Titulo dentro do site */}
+      <ListaTarefas /> {/* Construindo o listaTarefas */}
     </>
   );
 }
 
-export default App;
+export default App; //Exportando o app 
